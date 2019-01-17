b36a038f0607fc91faf75142b299c599c79f39aa
Erlend Dahl <erlend.dahl@sintef.no>
2019-01-17 14:35:05 +0100

    Added extensions method for chopping a string into substrings of certain lengths.

--- Extensions.Tests/StringExtensions.cs
+++ Extensions.Tests/StringExtensions.cs
@@ -439,5 +439,22 @@ All                                         44.0".RemoveEmptyLines();
             Assert.AreEqual(@"C:\...\file.txt", @"C:\folder\one\two\three\file.txt".TruncatePath(10));
             Assert.AreEqual(@"C:\...\file.txt", @"C:\folder\one\two\three\file.txt".TruncatePath(5));
         }
+
+        [TestMethod]
+        public void SubstringsOfLengthTests()
+        {
+            var abc = "abcdefghijklmnopqrstuvwxyz";
+            var strs = abc.SubstringsOfLength(1, 1, 2, 3, 4, 5).ToArray();
+            Assert.AreEqual("a", strs[0]);
+            Assert.AreEqual("b", strs[1]);
+            Assert.AreEqual("cd", strs[2]);
+            Assert.AreEqual("efg", strs[3]);
+            Assert.AreEqual("hijk", strs[4]);
+            Assert.AreEqual("lmnop", strs[5]);
+
+            strs = abc.SubstringsOfLength(10, 10).ToArray();
+            Assert.AreEqual("abcdefghij", strs[0]);
+            Assert.AreEqual("klmnopqrst", strs[1]);
+        }
     }
 }
--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -492,5 +492,21 @@ namespace Extensions
 
             return path;
         }
+
+        /// <summary>
+        /// Extracts and returns the string chopped into parts of the given lengths.
+        /// Example: "I am a string".SubstringsOfLength(4, 6, 1) => ["I am", " a str", "i"]
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="lengths"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> SubstringsOfLength(this string str, params int[] lengths)
+        {
+            foreach (var l in lengths)
+            {
+                yield return str.Substring(0, l);
+                str = str.Substring(l);
+            }
+        }
     }
 }
