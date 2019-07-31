6439a1f462af04a7f40c5c0a16bf19a384659eae
Erlend Dahl <erlend.dahl@sintef.no>
2019-07-31 13:15:31 +0200

    Added IncrementalStatistics class for calculating travelling statistics for huge lists.

---
+++ Extensions.Tests/Utilities/IncrementalStatisticsTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Extensions.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extensions.Tests.Utilities
+{
+    [TestClass]
+    public class IncrementalStatisticsTests
+    {
+        [TestMethod]
+        public void AverageTests()
+        {
+            var list = new[] { 0d, 1d, 2d, 3d, 4d, 5d, 5d, 5d, 5d, 5d, 10d };
+
+            var inc = new IncrementalStatistics();
+            for (var i = 0; i < list.Length; i++)
+            {
+                inc.AddObservation(list[i]);
+
+                var target = list.Take(i + 1).Average();
+                Assert.AreEqual(target, inc.Average);
+            }
+        }
+
+        [TestMethod]
+        public void MinTests()
+        {
+            var list = new[] { 0d, 1d, 2d, 3d, 4d, 5d, 5d, 5d, 5d, 5d, 10d };
+
+            var inc = new IncrementalStatistics();
+            for (var i = 0; i < list.Length; i++)
+            {
+                inc.AddObservation(list[i]);
+
+                var target = list.Take(i + 1).Min();
+                Assert.AreEqual(target, inc.Min);
+            }
+        }
+
+        [TestMethod]
+        public void MaxTests()
+        {
+            var list = new[] { 0d, 1d, 2d, 3d, 4d, 5d, 5d, 5d, 5d, 5d, 10d };
+
+            var inc = new IncrementalStatistics();
+            for (var i = 0; i < list.Length; i++)
+            {
+                inc.AddObservation(list[i]);
+
+                var target = list.Take(i + 1).Max();
+                Assert.AreEqual(target, inc.Max);
+            }
+        }
+
+        [TestMethod]
+        public void CountTests()
+        {
+            var list = new[] { 0d, 1d, 2d, 3d, 4d, 5d, 5d, 5d, 5d, 5d, 10d };
+
+            var inc = new IncrementalStatistics();
+            for (var i = 0; i < list.Length; i++)
+            {
+                inc.AddObservation(list[i]);
+                Assert.AreEqual(i + 1, inc.Count);
+            }
+        }
+
+        [TestMethod]
+        public void VarianceTests()
+        {
+            var list = new[] { 0d, 1d, 2d, 3d, 4d, 5d, 5d, 5d, 5d, 5d, 10d };
+
+            var inc = new IncrementalStatistics();
+            for (var i = 0; i < list.Length; i++)
+            {
+                inc.AddObservation(list[i]);
+
+                var target = list.Take(i + 1).Variance();
+                if (double.IsNaN(target)) Assert.IsTrue(double.IsNaN(inc.Variance));
+                else Assert.AreEqual(target, inc.Variance, 0.0000005);
+            }
+        }
+
+        [TestMethod]
+        public void StandardDeviationTests()
+        {
+            var list = new[] { 0d, 1d, 2d, 3d, 4d, 5d, 5d, 5d, 5d, 5d, 10d };
+
+            var inc = new IncrementalStatistics();
+            for (var i = 0; i < list.Length; i++)
+            {
+                inc.AddObservation(list[i]);
+
+                var target = list.Take(i + 1).StandardDeviation();
+                if (double.IsNaN(target)) Assert.IsTrue(double.IsNaN(inc.StandardDeviation));
+                else Assert.AreEqual(target, inc.StandardDeviation, 0.0000005);
+            }
+        }
+    }
+}
---
+++ Extensions/Utilities/IncrementalStatistics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.Utilities
+{
+    public class IncrementalStatistics
+    {
+        private double _variance;
+
+        public double Variance
+        {
+            get => Count < 2 ? double.NaN : _variance;
+            private set => _variance = value;
+        }
+
+        public double StandardDeviation => Math.Sqrt(Variance);
+        public double Average { get; private set; }
+        public double Min { get; private set; }
+        public double Max { get; private set; }
+        public int Count { get; private set; }
+
+        public void AddObservation(double observation)
+        {
+            Count++;
+            if (Count == 1)
+            {
+                Average = observation;
+                return;
+            }
+
+            var prevAverage = Average;
+            var prevVariance = _variance;
+
+            Average = (prevAverage * (Count - 1) + observation) / Count;
+            Variance = (Count - 2) * prevVariance / (Count - 1) + Math.Pow(observation - prevAverage, 2) / Count;
+            Min = Math.Min(Min, observation);
+            Max = Math.Max(Max, observation);
+        }
+    }
+}
