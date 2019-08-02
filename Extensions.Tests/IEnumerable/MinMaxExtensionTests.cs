3fd4278de97e31a2e5393f238dae8492b7ed5e80
Erlend Dahl <erlend.dahl@sintef.no>
2019-08-02 12:32:03 +0200

    Added an extension for getting both min and max values from a list with only one iteration.

---
+++ Extensions.Tests/IEnumerable/MinMaxExtensionTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Extensions.IEnumerable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extensions.Tests.IEnumerable
+{
+    [TestClass]
+    public class MinMaxExtensionTests
+    {
+        [TestMethod]
+        public void DoubleTests()
+        {
+            var items = new double[] { 4, 1, 5, 4, 2 };
+            var mm = items.MinMax();
+            Assert.AreEqual(1, mm.Min);
+            Assert.AreEqual(5, mm.Max);
+        }
+
+        [TestMethod]
+        public void IntTests()
+        {
+            var items = new int[] { 4, 1, 5, 4, 2 };
+            var mm = items.MinMax();
+            Assert.AreEqual(1, mm.Min);
+            Assert.AreEqual(5, mm.Max);
+        }
+
+        [TestMethod]
+        public void DoubleSelectorTests()
+        {
+            var items = new double[] { 4, 1, 5, 4, 2 };
+            var mm = items.MinMax(p => 2 * p);
+            Assert.AreEqual(2, mm.Min);
+            Assert.AreEqual(10, mm.Max);
+        }
+
+        [TestMethod]
+        public void IntSelectorTests()
+        {
+            var items = new int[] { 4, 1, 5, 4, 2 };
+            var mm = items.MinMax(p => 2 * p);
+            Assert.AreEqual(2, mm.Min);
+            Assert.AreEqual(10, mm.Max);
+        }
+    }
+}
---
+++ Extensions/IEnumerable/MinMaxExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.IEnumerable
+{
+    public struct IntegerMinMax
+    {
+        public int Min;
+        public int Max;
+    }
+
+    public struct DoubleMinMax
+    {
+        public double Min;
+        public double Max;
+    }
+
+    public static class MinMaxExtensions
+    {
+        public static DoubleMinMax MinMax<T>(this IEnumerable<T> enumerable, Func<T, double> selector)
+        {
+            var min = double.MaxValue;
+            var max = double.MinValue;
+            foreach (var item in enumerable)
+            {
+                var value = selector(item);
+                if (value < min) min = value;
+                if (value > max) max = value;
+            }
+
+            return new DoubleMinMax() { Min = min, Max = max };
+        }
+
+        public static DoubleMinMax MinMax(this IEnumerable<double> enumerable)
+        {
+            var min = double.MaxValue;
+            var max = double.MinValue;
+            foreach (var value in enumerable)
+            {
+                if (value < min) min = value;
+                if (value > max) max = value;
+            }
+
+            return new DoubleMinMax() { Min = min, Max = max };
+        }
+
+        public static IntegerMinMax MinMax<T>(this IEnumerable<T> enumerable, Func<T, int> selector)
+        {
+            var min = int.MaxValue;
+            var max = int.MinValue;
+            foreach (var item in enumerable)
+            {
+                var value = selector(item);
+                if (value < min) min = value;
+                if (value > max) max = value;
+            }
+
+            return new IntegerMinMax() { Min = min, Max = max };
+        }
+
+        public static IntegerMinMax MinMax(this IEnumerable<int> enumerable)
+        {
+            var min = int.MaxValue;
+            var max = int.MinValue;
+            foreach (var value in enumerable)
+            {
+                if (value < min) min = value;
+                if (value > max) max = value;
+            }
+
+            return new IntegerMinMax() { Min = min, Max = max };
+        }
+    }
+}
