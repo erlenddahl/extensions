3f022e44d8de9616a1e6b528b901396e944e809a
Erlend Dahl <erlend.dahl@sintef.no>
2019-10-29 09:18:37 +0100

    Added enum parsing helper.

---
+++ Extensions/Utilities/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.Utilities
+{
+    public static class EnumHelper
+    {
+        public static T ParseEnum<T>(string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+    }
+}
