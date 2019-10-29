7f3594259dd823b14c2b19de1b3fdfe58cd1172b
Erlend Dahl <erlend.dahl@sintef.no>
2019-10-29 09:19:16 +0100

    Added extension function for creating a dictionary from an IEnumerable with support for handling duplicate keys.

--- Extensions.Tests/DictionaryExtensions.cs
+++ Extensions.Tests/DictionaryExtensions.cs
@@ -61,5 +61,26 @@ namespace Extensions.Tests
             Assert.AreEqual(4, d.GetValueOrDefault("a"));
             Assert.AreEqual(2, d.GetValueOrDefault("b"));
         }
+
+        [TestMethod]
+        public void ToDictionarySafeTests()
+        {
+            var arr = new int[] { 1, 2, 3, 4, 5 };
+            var d = arr.ToDictionarySafe(k => k.ToString(), v => v);
+
+            Assert.AreEqual(5, d.Count);
+            for (var i = 1; i < 6; i++)
+                Assert.AreEqual(i, d[i.ToString()]);
+
+
+            arr = new int[] { 1, 2, 3, 4, 5, 5 };
+            d = arr.ToDictionarySafe(k => k.ToString(), v => v);
+
+            Assert.AreEqual(6, d.Count);
+            for (var i = 1; i < 6; i++)
+                Assert.AreEqual(i, d[i.ToString()]);
+
+            Assert.AreEqual(5, d["5 (1)"]);
+        }
     }
 }
--- Extensions/DictionaryExtensions.cs
+++ Extensions/DictionaryExtensions.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 
 namespace Extensions
 {
@@ -261,6 +262,36 @@ namespace Extensions
             if (!dict.Has(key)) return new byte[0];
             if (_debug) Debug.WriteLine(key + ": " + dict[key].GetType());
             return (byte[]) dict[key];
+        }
+
+        /// <summary>
+        /// Creates a dictionary from the given enumerable, using the key function to extract keys, and the value function to extract values.
+        /// Works like the normal ToDictionary, but if there are duplicate keys, this version will solve the issue by appending (1), (2), etc
+        /// to the keys until they are unique.
+        /// </summary>
+        /// <typeparam name="TI"></typeparam>
+        /// <typeparam name="TV"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="keyFunc"></param>
+        /// <param name="valueFunc"></param>
+        /// <returns></returns>
+        public static Dictionary<string, TV> ToDictionarySafe<TI, TV>(this IEnumerable<TI> list, Func<TI, string> keyFunc, Func<TI, TV> valueFunc)
+        {
+            var dict = new Dictionary<string, TV>();
+            foreach (var element in list)
+            {
+                var key = keyFunc(element);
+                var value = valueFunc(element);
+
+                var safeKey = key;
+                var safeNumber = 1;
+                while (dict.ContainsKey(safeKey))
+                    safeKey = key + " (" + safeNumber++ + ")";
+
+                dict.Add(safeKey, value);
+            }
+
+            return dict;
         }
     }
 }
