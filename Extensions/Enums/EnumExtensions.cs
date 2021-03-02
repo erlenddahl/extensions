b7593d2d851d18f0253689b8217f6de0acb137ef
Erlend Dahl <erlend.dahl@sintef.no>
2021-03-02 08:49:37 +0100

    Added basic enum extensions.

---
+++ Extensions/Enums/EnumExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.Enums
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Gets all items for an enum value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static IEnumerable<T> GetAllItems<T>(this Enum value)
+        {
+            foreach (var item in Enum.GetValues(typeof(T)))
+            {
+                yield return (T) item;
+            }
+        }
+
+        /// <summary>
+        /// Gets all items for an enum type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<T> GetAllItems<T>() where T : Enum
+        {
+            foreach (object item in Enum.GetValues(typeof(T)))
+            {
+                yield return (T)item;
+            }
+        }
+    }
+}
