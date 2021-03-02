3d6fa44f7046cd9f2d7dac1d1baf016751f2f156
Erlend Dahl <erlend.dahl@sintef.no>
2021-03-02 08:57:38 +0100

    Added very simple CSV writer.

--- Extensions/CsvReader.cs
+++ Extensions/CsvReader.cs
@@ -7,6 +7,22 @@ using System.Threading.Tasks;
 
 namespace Extensions
 {
+    public class CsvWriter
+    {
+        private readonly string _separator;
+
+        public CsvWriter(string separator)
+        {
+            _separator = separator;
+        }
+
+        public string QuoteValue(string value)
+        {
+            if (value.Contains(_separator)) return "\"" + value + "\"";
+            return value;
+        }
+    }
+
     public class CsvReader
     {
         private char _separator;
