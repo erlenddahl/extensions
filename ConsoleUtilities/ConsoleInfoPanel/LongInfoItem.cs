649ed79ae3ad899c30371cb4e63dfab566732701
Erlend Dahl <erlend.dahl@gmail.com>
2022-02-04 15:21:30 +0100

    Missing file.

---
+++ ConsoleUtilities/ConsoleInfoPanel/LongInfoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleUtilities.ConsoleInfoPanel
+{
+    public class LongInfoItem : ConsoleInfoItem
+    {
+        public long Value;
+        public string FormatString = "n0";
+
+        public override string Format(int consoleWidth)
+        {
+            return Value.ToString(FormatString);
+        }
+    }
+}
