8d2b63fe2d11a38c80b96c79e597b16a413cefe9
Erlend Dahl <erlend.dahl@sintef.no>
2022-09-21 16:01:38 +0200

    Added CIP.Run for IEnumerables.

--- ConsoleUtilities/ConsoleInfoPanel/ConsoleInformationPanelExtensions.cs
+++ ConsoleUtilities/ConsoleInfoPanel/ConsoleInformationPanelExtensions.cs
@@ -16,6 +16,22 @@ namespace ConsoleUtilities.ConsoleInfoPanel
         /// <returns></returns>
         public static IEnumerable<T> Run<T>(this ConsoleInformationPanel cip, string key, ICollection<T> items, int? sequence = null)
         {
+            return cip.Run(key, items, items.Count, sequence);
+        }
+
+        /// <summary>
+        /// Enumerates the given items. If the cip object is null, this acts as a normal enumerator. Otherwise, it creates
+        /// and updates a progressbar with the given key.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="cip"></param>
+        /// <param name="key"></param>
+        /// <param name="items"></param>
+        /// <param name="count"></param>
+        /// <param name="sequence"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Run<T>(this ConsoleInformationPanel cip, string key, IEnumerable<T> items, int count, int? sequence = null)
+        {
             if (cip == null)
             {
                 foreach (var item in items)
@@ -23,13 +39,39 @@ namespace ConsoleUtilities.ConsoleInfoPanel
                 yield break;
             }
 
-            using (var pb = cip.SetProgress(key, max: items.Count, sequence: sequence))
+            using (var pb = cip.SetProgress(key, max: count, sequence: sequence))
             {
                 foreach (var item in items)
                 {
                     yield return item;
                     pb.Increment();
                 }
+            }
+        }
+
+        /// <summary>
+        /// Enumerates the given items. If the cip object is null, this acts as a normal enumerator. Otherwise, it creates
+        /// and updates a progressbar with the given key.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="cip"></param>
+        /// <param name="key"></param>
+        /// <param name="items"></param>
+        /// <param name="sequence"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Run<T>(this ConsoleInformationPanel cip, string key, IEnumerable<T> items, int? sequence = null)
+        {
+            if (cip == null)
+            {
+                foreach (var item in items)
+                    yield return item;
+                yield break;
+            }
+
+            using (var pb = cip.SetUnknownProgress(key, sequence: sequence))
+            {
+                foreach (var item in items)
+                    yield return item;
             }
         }
     }
