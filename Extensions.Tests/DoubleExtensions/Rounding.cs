7ceb3f1b4219d74f107995dc32877de3285250e2
Erlend Dahl <erlend.dahl@sintef.no>
2022-09-30 10:17:56 +0200

    Added a more flexible bucket rounding function for doubles.

--- Extensions.Tests/DoubleExtensions/Rounding.cs
+++ Extensions.Tests/DoubleExtensions/Rounding.cs
@@ -1,4 +1,5 @@
-﻿using Extensions.DoubleExtensions;
+﻿using Extensions.DateTimeExtensions;
+using Extensions.DoubleExtensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Extensions.Tests.DoubleExtensions
@@ -25,6 +26,33 @@ namespace Extensions.Tests.DoubleExtensions
 
             Assert.AreEqual(0, 7.0.RoundToNearest(100));
             Assert.AreEqual(100, 55.0.RoundToNearest(100));
+        }
+
+        [TestMethod]
+        public void RoundDown()
+        {
+            Assert.AreEqual(0, 4.3.Round(10, RoundingDirection.Down));
+            Assert.AreEqual(0, 0.3.Round(10, RoundingDirection.Down));
+            Assert.AreEqual(0, 9.3.Round(10, RoundingDirection.Down));
+            Assert.AreEqual(0, 6.3.Round(10, RoundingDirection.Down));
+        }
+
+        [TestMethod]
+        public void RoundUp()
+        {
+            Assert.AreEqual(10, 4.3.Round(10, RoundingDirection.Up));
+            Assert.AreEqual(10, 0.3.Round(10, RoundingDirection.Up));
+            Assert.AreEqual(10, 9.3.Round(10, RoundingDirection.Up));
+            Assert.AreEqual(10, 6.3.Round(10, RoundingDirection.Up));
+        }
+
+        [TestMethod]
+        public void RoundNearest()
+        {
+            Assert.AreEqual(0, 4.3.Round(10, RoundingDirection.Nearest));
+            Assert.AreEqual(0, 0.3.Round(10, RoundingDirection.Nearest));
+            Assert.AreEqual(10, 9.3.Round(10, RoundingDirection.Nearest));
+            Assert.AreEqual(10, 6.3.Round(10, RoundingDirection.Nearest));
         }
     }
 }
--- Extensions/DoubleExtensions/Rounding.cs
+++ Extensions/DoubleExtensions/Rounding.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Extensions.DateTimeExtensions;
 
 namespace Extensions.DoubleExtensions
 {
     public static class Rounding
     {
-
         /// <summary>
         /// Rounds the given number to the nearest bucket value.
         /// Examples:
@@ -20,8 +20,32 @@ namespace Extensions.DoubleExtensions
         /// <returns></returns>
         public static int RoundToNearest(this double num, int bucket, MidpointRounding rounding = MidpointRounding.ToEven)
         {
-            var rounded = (int) Math.Round(num / (double) bucket, rounding);
+            var rounded = (int)Math.Round(num / (double)bucket, rounding);
             return rounded * bucket;
+        }
+
+        /// <summary>
+        /// Rounds the given number to the nearest bucket value.
+        /// Examples:
+        ///     12.Round(10, RoundingDirection.Up) => 20
+        ///     16.Round(10, RoundingDirection.Down) => 10
+        ///     12.Round(10, RoundingDirection.Nearest) => 10
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="bucket"></param>
+        /// <param name="direction"></param>
+        /// <param name="rounding"></param>
+        /// <returns></returns>
+        public static int Round(this double num, int bucket, RoundingDirection direction = RoundingDirection.Nearest, MidpointRounding rounding = MidpointRounding.ToEven)
+        {
+            if (direction == RoundingDirection.Nearest)
+                return num.RoundToNearest(bucket, rounding);
+
+            var number = (int)Math.Round(num, 0, rounding);
+            var modulo = number % bucket;
+
+            if (direction == RoundingDirection.Down) return number - modulo;
+            return number + (bucket - modulo);
         }
     }
 }
