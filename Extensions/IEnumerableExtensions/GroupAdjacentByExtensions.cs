3211bd32f62c91c3d956c3d3715e2152ef363a81
Erlend Dahl <erlend.dahl@sintef.no>
2022-09-30 10:24:42 +0200

    Added a few overloads with custom merge functions.

--- Extensions/IEnumerableExtensions/GroupAdjacentByExtensions.cs
+++ Extensions/IEnumerableExtensions/GroupAdjacentByExtensions.cs
@@ -30,5 +30,59 @@ namespace Extensions.IEnumerableExtensions
                 }
             }
         }
+
+        public static IEnumerable<TOut> GroupAdjacentBy<TIn, TOut>(this IEnumerable<TIn> source, Func<TIn, TIn, bool> predicate, Func<List<TIn>, TOut> mergeFunc)
+        {
+            using (var e = source.GetEnumerator())
+            {
+                if (e.MoveNext())
+                {
+                    var list = new List<TIn> { e.Current };
+                    var pred = e.Current;
+                    while (e.MoveNext())
+                    {
+                        if (predicate(pred, e.Current))
+                        {
+                            list.Add(e.Current);
+                        }
+                        else
+                        {
+                            yield return mergeFunc(list);
+                            list.Clear();
+                            list.Add(e.Current);
+                        }
+                        pred = e.Current;
+                    }
+                    yield return mergeFunc(list);
+                }
+            }
+        }
+
+        public static IEnumerable<TOut> GroupAdjacentBy<TIn, TOut>(this IEnumerable<TIn> source, Func<TIn, object> comparer, Func<List<TIn>, TOut> mergeFunc)
+        {
+            using (var e = source.GetEnumerator())
+            {
+                if (e.MoveNext())
+                {
+                    var list = new List<TIn> { e.Current };
+                    var pred = e.Current;
+                    while (e.MoveNext())
+                    {
+                        if (comparer(pred).Equals(comparer(e.Current)))
+                        {
+                            list.Add(e.Current);
+                        }
+                        else
+                        {
+                            yield return mergeFunc(list);
+                            list.Clear();
+                            list.Add(e.Current);
+                        }
+                        pred = e.Current;
+                    }
+                    yield return mergeFunc(list);
+                }
+            }
+        }
     }
 }
