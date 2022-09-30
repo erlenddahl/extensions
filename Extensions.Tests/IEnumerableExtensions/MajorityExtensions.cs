48369e4cb83cbaea241591e7ca6e1e4eecc48ceb
Erlend Dahl <erlend.dahl@sintef.no>
2022-09-30 10:20:07 +0200

    Added a majority function that picks the majority (most common) element from the given list.

---
+++ Extensions.Tests/IEnumerableExtensions/MajorityExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Extensions.IEnumerableExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extensions.Tests.IEnumerableExtensions
+{
+    [TestClass]
+    public class MajorityExtensions
+    {
+        [TestMethod]
+        public void AllIdentical()
+        {
+            var l = new[] { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 };
+            Assert.AreEqual(3, l.Majority());
+        }
+
+        [TestMethod]
+        public void ClearMajority()
+        {
+            var l = new[] { 3, 3, 3, 2, 3, 3, 3, 3, 3, 3 };
+            Assert.AreEqual(3, l.Majority());
+        }
+
+        [TestMethod]
+        public void SlightMajority()
+        {
+            var l = new[] { 3, 2, 3, 2, 1, 3, 1, 2, 1, 3 };
+            Assert.AreEqual(3, l.Majority());
+        }
+
+        [TestMethod]
+        public void Tie()
+        {
+            var l = new[] { 1, 4, 1, 4, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 1, 4, 6, 55, 6 };
+            var m = l.Majority();
+            if (m != 2 && m != 3)
+                Assert.Fail();
+        }
+
+        [TestMethod]
+        public void EmptyList()
+        {
+            var l = Array.Empty<int>();
+            try
+            {
+                l.Majority();
+                Assert.Fail();
+            }
+            catch (Exception tex)
+            {
+                Console.WriteLine(tex.Message);
+            }
+        }
+    }
+}
---
+++ Extensions/IEnumerableExtensions/MajorityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Extensions.DictionaryExtensions;
+
+namespace Extensions.IEnumerableExtensions
+{
+    public static class MajorityExtensions
+    {
+        public static T Majority<T>(this IEnumerable<T> list)
+        {
+            var counts = new Dictionary<T, int>();
+            foreach (var item in list)
+                counts.Increment(item);
+            return counts.MaxBy(p => p.Value).Key;
+        }
+    }
+}
