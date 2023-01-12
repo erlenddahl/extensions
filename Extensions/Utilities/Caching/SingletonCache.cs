b889ddef3cc3ea6a0a4e081b2d53544f7a36c428
Erlend Dahl <erlend.dahl@sintef.no>
2023-01-12 12:55:51 +0100

    Added a cache helper for re-using singletons without creating them before it's necessary.

---
+++ Extensions/Utilities/Caching/SingletonCache.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensions.Utilities.Caching
+{
+    /// <summary>
+    /// Simple dictionary based cache meant for lazily created singleton values.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class SingletonCache<TKey, TValue>
+    {
+        private readonly Dictionary<TKey, TValue> _cache = new Dictionary<TKey, TValue>();
+        private readonly object _locker = new object();
+
+        /// <summary>
+        /// Returns the cached object corresponding to the given key if it exists, or
+        /// creates it using the valueCreationFunc, and then adds it to the cache before
+        /// returning it.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="valueCreationFunc"></param>
+        /// <returns></returns>
+        public TValue Get(TKey key, Func<TValue> valueCreationFunc)
+        {
+            lock (_locker)
+            {
+                if (_cache.TryGetValue(key, out var cached)) return cached;
+                cached = valueCreationFunc();
+                _cache.Add(key, cached);
+                return cached;
+            }
+        }
+    }
+}
