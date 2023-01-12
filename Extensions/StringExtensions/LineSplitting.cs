1afb7d90d55ce500ef6e9931f4c790dd13f75f7c
Erlend Dahl <erlend.dahl@sintef.no>
2023-01-12 12:55:25 +0100

    Implemented function for splitting a string by lines.

---
+++ Extensions/StringExtensions/LineSplitting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions.StringExtensions
+{
+    public static class LineSplitting
+    {
+        public static IEnumerable<string> Lines(this string str)
+        {
+            using (System.IO.StringReader reader = new System.IO.StringReader(str))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
