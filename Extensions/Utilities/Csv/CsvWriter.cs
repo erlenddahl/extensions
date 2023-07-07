b8a4ccddfb54a4c6d91ef41f84254d6e00c36f76
Erlend Dahl <erlend.dahl@gmail.com>
2023-07-07 09:38:04 +0200

    Csv Writer can now write lists of objects (single-depth).

--- Extensions/Utilities/Csv/CsvWriter.cs
+++ Extensions/Utilities/Csv/CsvWriter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 
@@ -37,6 +38,27 @@ namespace Extensions.Utilities.Csv
             return string.Join(_separator, values.Select(QuoteValue));
         }
 
+        public string QuoteValues(IEnumerable<object> values)
+        {
+            return string.Join(_separator, values.Select(p => QuoteValue(Stringify(p))));
+        }
+
+        public static string Stringify(object o, IFormatProvider provider = null, string numericFormat = null, string datetimeFormat = null)
+        {
+            if (o is string s) return s;
+            if (o is short sh) return sh.ToString();
+            if (o is int i) return i.ToString();
+            if (o is long l) return l.ToString();
+            if (o is ushort ush) return ush.ToString();
+            if (o is uint ui) return ui.ToString();
+            if (o is ulong ul) return ul.ToString();
+            if (o is float f) return f.ToString(numericFormat, provider ?? CultureInfo.InvariantCulture);
+            if (o is double d) return d.ToString(numericFormat, provider ?? CultureInfo.InvariantCulture);
+            if (o is decimal de) return de.ToString(numericFormat, provider ?? CultureInfo.InvariantCulture);
+            if (o is DateTime dt) return dt.ToString(datetimeFormat ?? "yyyy-MM-dd HH:mm:ss.fff");
+            throw new NotImplementedException("CsvWriter has no Stringify implementation for type " + o.GetType());
+        }
+
         public void WriteLine(IEnumerable<string> values)
         {
             if (_targetFile == null) _targetFile = new StreamWriter(_targetPath);
@@ -44,6 +66,15 @@ namespace Extensions.Utilities.Csv
         }
 
         public void WriteAllLines(IEnumerable<IEnumerable<string>> contents)
+        {
+            using (var targetFile = _targetFile ?? new StreamWriter(_targetPath))
+            {
+                foreach (var line in contents)
+                    targetFile.WriteLine(QuoteValues(line));
+            }
+        }
+
+        public void WriteAllLines(IEnumerable<IEnumerable<object>> contents)
         {
             using (var targetFile = new StreamWriter(_targetPath))
             {
