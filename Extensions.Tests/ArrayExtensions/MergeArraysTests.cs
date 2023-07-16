d3532fe44109415f6313a6cc8f4cfdcbc9fdb24e
Erlend Dahl <erlend.dahl@gmail.com>
2023-07-16 07:10:49 +0200

    Added functions for quickly merging arrays using Array.Copy.

---
+++ Extensions.Tests/ArrayExtensions/MergeArraysTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using Extensions.ArrayExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extensions.Tests.ArrayExtensions
+{
+    [TestClass]
+    public class MergeArraysTests
+    {
+        [TestMethod]
+        public void Two()
+        {
+            var arr = new[] { 1, 2, 3, 4, 5 };
+            var arr2 = new[] { 6, 7, 8, 9, 10 };
+
+            CollectionAssert.AreEqual(arr.Concat(arr2).ToArray(), arr.Merge(arr2));
+        }
+
+        [TestMethod]
+        public void Two_FirstEmpty()
+        {
+            var arr = Array.Empty<int>();
+            var arr2 = new[] { 6, 7, 8, 9, 10 };
+
+            CollectionAssert.AreEqual(arr.Concat(arr2).ToArray(), arr.Merge(arr2));
+        }
+        [TestMethod]
+        public void Two_SecondEmpty()
+        {
+            var arr = new[] { 1, 2, 3, 4, 5 };
+            var arr2 = Array.Empty<int>();
+
+            CollectionAssert.AreEqual(arr.Concat(arr2).ToArray(), arr.Merge(arr2));
+        }
+
+        [TestMethod]
+        public void Three()
+        {
+            var arr = new[] { 1, 2, 3, 4, 5 };
+            var arr2 = new[] { 6, 7, 8, 9, 10 };
+            var arr3 = new[] { 11, 12, 13 };
+
+            CollectionAssert.AreEqual(arr.Concat(arr2).Concat(arr3).ToArray(), arr.Merge(arr2, arr3));
+        }
+
+        [TestMethod]
+        public void Four()
+        {
+            var arr = new[] { 1, 2, 3, 4, 5 };
+            var arr2 = new[] { 6, 7, 8, 9, 10 };
+            var arr3 = new[] { 11, 12, 13 };
+            var arr4 = new[] { 14, 15, 16, 17 };
+
+            CollectionAssert.AreEqual(arr.Concat(arr2).Concat(arr3).Concat(arr4).ToArray(), arr.Merge(arr2, arr3, arr4));
+        }
+
+        [TestMethod]
+        public void Five()
+        {
+            var arr = new[] { 1, 2, 3, 4, 5 };
+            var arr2 = new[] { 6, 7, 8, 9, 10 };
+            var arr3 = new[] { 11, 12, 13 };
+            var arr4 = new[] { 14, 15, 16, 17 };
+            var arr5 = new[] { 18, 19, 20 };
+
+            CollectionAssert.AreEqual(arr.Concat(arr2).Concat(arr3).Concat(arr4).Concat(arr5).ToArray(), arr.Merge(arr2, arr3, arr4, arr5));
+        }
+
+        [TestMethod]
+        public void Five_OneEmpty()
+        {
+            var arr = new[] { 1, 2, 3, 4, 5 };
+            var arr2 = new[] { 6, 7, 8, 9, 10 };
+            var arr3 = new[] { 11, 12, 13 };
+            var arr4 = Array.Empty<int>();
+            var arr5 = new[] { 18, 19, 20 };
+
+            CollectionAssert.AreEqual(arr.Concat(arr2).Concat(arr3).Concat(arr4).Concat(arr5).ToArray(), arr.Merge(arr2, arr3, arr4, arr5));
+        }
+    }
+}
---
+++ Extensions/ArrayExtensions/MergeArrays.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Extensions.ArrayExtensions
+{
+    public static class MergeArrays
+    {
+        public static T[] Merge<T>(this T[] a, T[] b)
+        {
+            var result = new T[a.Length + b.Length];
+            Array.Copy(a, 0, result, 0, a.Length);
+            Array.Copy(b, 0, result, a.Length, b.Length);
+            return result;
+        }
+
+        public static T[] Merge<T>(this T[] a, T[] b, T[] c)
+        {
+            var result = new T[a.Length + b.Length + c.Length];
+            Array.Copy(a, 0, result, 0, a.Length);
+            Array.Copy(b, 0, result, a.Length, b.Length);
+            Array.Copy(c, 0, result, a.Length + b.Length, c.Length);
+            return result;
+        }
+
+        public static T[] Merge<T>(this T[] a, T[] b, T[] c, T[] d)
+        {
+            var result = new T[a.Length + b.Length + c.Length + d.Length];
+            Array.Copy(a, 0, result, 0, a.Length);
+            Array.Copy(b, 0, result, a.Length, b.Length);
+            Array.Copy(c, 0, result, a.Length + b.Length, c.Length);
+            Array.Copy(d, 0, result, a.Length + b.Length + c.Length, d.Length);
+            return result;
+        }
+
+        public static T[] Merge<T>(this T[] a, T[] b, T[] c, T[] d, T[] e)
+        {
+            var result = new T[a.Length + b.Length + c.Length + d.Length + e.Length];
+            Array.Copy(a, 0, result, 0, a.Length);
+            Array.Copy(b, 0, result, a.Length, b.Length);
+            Array.Copy(c, 0, result, a.Length + b.Length, c.Length);
+            Array.Copy(d, 0, result, a.Length + b.Length + c.Length, d.Length);
+            Array.Copy(e, 0, result, a.Length + b.Length + c.Length + d.Length, e.Length);
+            return result;
+        }
+    }
+}
