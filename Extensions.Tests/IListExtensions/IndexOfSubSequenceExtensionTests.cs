aeefc9f8cae207c7bbc2326e03a83e86f55a76e0
Erlend Dahl <erlend.dahl@gmail.com>
2023-07-16 07:10:31 +0200

    Added function for finding the index of a sub array in an array.

---
+++ Extensions.Tests/IListExtensions/IndexOfSubSequenceExtensionTests.cs
@@ -0,0 +1,49 @@
+﻿using Extensions.IListExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extensions.Tests.IListExtensions
+{
+    [TestClass]
+    public class IndexOfSubSequenceExtensionTests
+    {
+        [TestMethod]
+        public void AtStartOfList()
+        {
+            var list = new[] { 1, 3, 2, 1, 2, 3, 4, 5 };
+            var ix = list.IndexOf(new[] { 1, 3, 2 });
+            Assert.AreEqual(0, ix);
+        }
+
+        [TestMethod]
+        public void MiddleOfList()
+        {
+            var list = new[] { 1, 3, 2, 1, 2, 3, 4, 5 };
+            var ix = list.IndexOf(new[] { 2, 1, 2 });
+            Assert.AreEqual(2, ix);
+        }
+
+        [TestMethod]
+        public void EndOfList()
+        {
+            var list = new[] { 1, 3, 2, 1, 2, 3, 4, 5 };
+            var ix = list.IndexOf(new[] { 2, 3, 4, 5 });
+            Assert.AreEqual(4, ix);
+        }
+
+        [TestMethod]
+        public void SkipAtStartOfList()
+        {
+            var list = new[] { 1, 3, 2, 1, 2, 3, 4, 1, 3, 2, 5 };
+            var ix = list.IndexOf(new[] { 1, 3, 2 }, 3);
+            Assert.AreEqual(7, ix);
+        }
+
+        [TestMethod]
+        public void NotFound()
+        {
+            var list = new[] { 1, 3, 2, 1, 2, 3, 4, 1, 3, 2, 5 };
+            var ix = list.IndexOf(new[] { 1, 3, 2 }, 99);
+            Assert.AreEqual(-1, ix);
+        }
+    }
+}
---
+++ Extensions/IListExtensions/IndexOfSubSequenceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensions.IListExtensions
+{
+    public static class IndexOfSubSequenceExtensions
+    {
+        public static int IndexOf<T>(this IList<T> haystack, IList<T> needle, int startAt = 0)
+        {
+            var len = needle.Count;
+            var limit = haystack.Count - len;
+            for (var i = startAt; i <= limit; i++)
+            {
+                var k = 0;
+                for (; k < len; k++)
+                {
+                    if (!needle[k].Equals(haystack[i + k])) break;
+                }
+                if (k == len) return i;
+            }
+            return -1;
+        }
+    }
+}
