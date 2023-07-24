ad9c1c68c0484c1f2031f4ffdfd866c1ef07c825
Erlend Dahl <erlend.dahl@gmail.com>
2023-07-24 11:08:01 +0200

    Added functionality for computing the least common multiple of a number from a generic list.

---
+++ Extensions.Tests/IEnumerableExtensions/LeastCommonMultipleExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Extensions.IEnumerableExtensions;
+
+namespace Extensions.Tests.IEnumerableExtensions
+{
+    [TestClass]
+    public class LeastCommonMultipleExtensions
+    {
+        [TestMethod]
+        public void ComputeLCM_SingleNumber_ShouldReturnSameNumber()
+        {
+            var numbers = new List<int> { 7 };
+            var result = numbers.ComputeLeastCommonMultiple(n => n);
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void ComputeLCM_TwoNumbers_ShouldReturnLCM()
+        {
+            var numbers = new List<int> { 12, 18 };
+            var result = numbers.ComputeLeastCommonMultiple(n => n);
+            Assert.AreEqual(36, result);
+        }
+
+        [TestMethod]
+        public void ComputeLCM_MultipleNumbers_ShouldReturnLCM()
+        {
+            var numbers = new List<int> { 2, 5, 10, 15, 20 };
+            var result = numbers.ComputeLeastCommonMultiple(n => n);
+            Assert.AreEqual(60, result);
+        }
+
+        [TestMethod]
+        public void ComputeLCM_EqualNumbers_ShouldReturnSameNumber()
+        {
+            var numbers = new List<int> { 5, 5, 5, 5, 5 };
+            var result = numbers.ComputeLeastCommonMultiple(n => n);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void ComputeLCM_NonEqualNumbers_ShouldReturnLCM()
+        {
+            var numbers = new List<int> { 3, 7, 14, 21 };
+            var result = numbers.ComputeLeastCommonMultiple(n => n);
+            Assert.AreEqual(42, result);
+        }
+    }
+}
---
+++ Extensions/IEnumerableExtensions/LeastCommonMultipleExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Extensions.IEnumerableExtensions
+{
+    public static class LeastCommonMultipleExtensions
+    {
+        public static int ComputeLeastCommonMultiple<T>(this IEnumerable<T> list, Func<T, int> valueGetter)
+        {
+            if (list == null || !list.Any())
+                throw new ArgumentException("The list cannot be null or empty.");
+
+            return InternalComputeLeastCommonMultiple(list.Select(valueGetter));
+        }
+
+        public static int ComputeLeastCommonMultiple(this IEnumerable<int> list)
+        {
+            return InternalComputeLeastCommonMultiple(list);
+        }
+
+        // Helper function to compute the least common multiple of an array of integers
+        private static int InternalComputeLeastCommonMultiple(IEnumerable<int> numbers)
+        {
+            var lcm = -1;
+            foreach (var n in numbers)
+            {
+                if (lcm == -1)
+                {
+                    lcm = n;
+                    continue;
+                }
+                lcm = LeastCommonMultiple(lcm, n);
+            }
+            return lcm;
+        }
+
+        // Helper function to compute the least common multiple of two integers using the greatest common divisor (GCD)
+        private static int LeastCommonMultiple(int a, int b)
+        {
+            return Math.Abs(a * b) / GreatestCommonDivisor(a, b);
+        }
+
+        // Helper function to compute the greatest common divisor (GCD) of two integers using Euclid's algorithm
+        private static int GreatestCommonDivisor(int a, int b)
+        {
+            while (b != 0)
+            {
+                var temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
