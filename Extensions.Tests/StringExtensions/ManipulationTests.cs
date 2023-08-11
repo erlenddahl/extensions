496a7a22eea71429c3b83cfcf8d28d3365dfadcd
Erlend Dahl <erlend.dahl@gmail.com>
2023-08-11 14:15:53 +0200

    Added max length helper.

---
+++ Extensions.Tests/StringExtensions/ManipulationTests.cs
@@ -0,0 +1,19 @@
+﻿using Extensions.StringExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extensions.Tests.StringExtensions
+{
+    [TestClass]
+    public class ManipulationTests
+    {
+
+        [TestMethod]
+        public void MaxLengthTests()
+        {
+            Assert.AreEqual("This is a long sentence.", "This is a long sentence.".MaxLength(100, "..."));
+            Assert.AreEqual("This is a long sentence.", "This is a long sentence.".MaxLength(24, "..."));
+            Assert.AreEqual("This is a ", "This is a long sentence.".MaxLength(10));
+            Assert.AreEqual("This is a ...", "This is a long sentence.".MaxLength(10, "..."));
+        }
+    }
+}
---
+++ Extensions/StringExtensions/Manipulation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Extensions.StringExtensions
+{
+    public static class Manipulation
+    {
+
+        /// <summary>
+        /// Shortens the given string if it is longer than the given limit,
+        /// and optionally inserts a suffix to indicate this.
+        /// Example: "I am a string".MaxLength(4, "[...]") => "I am [...]"
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="limit"></param>
+        /// <param name="suffix"></param>
+        /// <returns></returns>
+        public static string MaxLength(this string str, int limit, string suffix="")
+        {
+            if (str.Length <= limit) return str;
+            return str.Substring(0, limit) + suffix;
+        }
+    }
+}
