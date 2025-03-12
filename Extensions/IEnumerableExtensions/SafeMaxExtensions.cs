008e92d97bb97084ddf5e2b0eccef2bd720037a2
Erlend Dahl <erlend.dahl@gmail.com>
2025-03-12 09:18:40 +0100

    Added SafeMax for datetime.

--- Extensions/IEnumerableExtensions/SafeMaxExtensions.cs
+++ Extensions/IEnumerableExtensions/SafeMaxExtensions.cs
@@ -31,5 +31,19 @@ namespace Extensions.IEnumerableExtensions
             if (source == null || !source.Any()) return defaultValue;
             return source.Max(target);
         }
+
+        /// <summary>
+        /// Does exactly the same as the Max() function, but returns a default value if the source collection is null or empty.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static DateTime SafeMax<T>(this IEnumerable<T> source, Func<T, DateTime> target, DateTime? defaultValue = null)
+        {
+            if (source == null || !source.Any()) return defaultValue ?? DateTime.MinValue;
+            return source.Max(target);
+        }
     }
 }
