78fe878798e7fc0a2c59ecd8399941c2490a6e1a
Erlend Dahl <erlend.dahl@sintef.no>
2026-03-13 14:05:31 +0100

    Converted to SDK-style test project and updated MSTest package references

--- NpgsqlExtensions.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("115339b8-2e4b-426c-9112-73c0af7d292a")]
