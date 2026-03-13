e9c27e5e6478ef4dd9a8109ef19a0ae72cf50580
Erlend Dahl <erlend.dahl@sintef.no>
2026-03-13 14:05:03 +0100

    Move assembly metadata to project file
    Assembly metadata such as title, product, copyright, and version information was moved from AssemblyInfo.cs to the NpgsqlExtensions.Tests.csproj file. The redundant attributes were removed from AssemblyInfo.cs, streamlining project configuration and centralizing metadata management in the project file.

--- NpgsqlExtensions.Tests/Properties/AssemblyInfo.cs
+++ NpgsqlExtensions.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NpgsqlExtensions.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NpgsqlExtensions.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("115339b8-2e4b-426c-9112-73c0af7d292a")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
