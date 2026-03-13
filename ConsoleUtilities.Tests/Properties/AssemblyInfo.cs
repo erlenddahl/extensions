911ebbfd98653956831fa7cafccc0b33aee025d9
Erlend Dahl <erlend.dahl@sintef.no>
2026-03-13 13:57:34 +0100

    Move assembly metadata to project file for ConsoleUtilities.Tests
    Assembly metadata such as title, product, copyright, and version information was moved from Properties/AssemblyInfo.cs to the ConsoleUtilities.Tests.csproj file. This streamlines project configuration and aligns with modern .NET practices. The AssemblyInfo.cs file now contains only essential attributes.

--- ConsoleUtilities.Tests/Properties/AssemblyInfo.cs
+++ ConsoleUtilities.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ConsoleUtilities.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleUtilities.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("c90cd123-5da5-43e7-a16c-3ded63032c00")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
