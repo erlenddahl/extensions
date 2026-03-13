e29f24be2e73d7cba9489a5c5fb3e709f8ea9321
Erlend Dahl <erlend.dahl@sintef.no>
2026-03-13 13:56:09 +0100

    Move assembly metadata to project file
    Assembly metadata such as title, product, copyright, and version information was moved from AssemblyInfo.cs to ByteUtilities.Tests.csproj. The corresponding attributes were removed from AssemblyInfo.cs, centralizing metadata management in the project file. This streamlines configuration and aligns with modern .NET project practices.

--- ByteUtilities.Tests/Properties/AssemblyInfo.cs
+++ ByteUtilities.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ByteUtilities.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ByteUtilities.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("d461820b-d95c-4e09-b2c2-91672eb659c3")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
