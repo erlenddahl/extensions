f0986ace6e2d15b3b55dc93869521ed478617326
Erlend Dahl <erlend.dahl@sintef.no>
2026-03-13 13:58:11 +0100

    Converted to SDK-style with MSTest packages

--- ConsoleUtilities.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("c90cd123-5da5-43e7-a16c-3ded63032c00")]
